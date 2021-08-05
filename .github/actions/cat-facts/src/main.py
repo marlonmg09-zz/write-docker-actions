118fa76b1fbb9227fdb697efff39e8ec809dcf16
Marlon Mejia <marlonme.09@hotmail.com>
2021-08-04 20:34:07 -0500

    Modification of the logic for fetching the facts

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -3,9 +3,37 @@ import random
 import sys
 
 # Make an HTTP GET request to the cat-fact API
-cat_url = "https://cat-fact.herokuapp.com/facts"
-r = requests.get(cat_url)
-r_obj_list = r.json()
+# cat_url = "https://cat-fact.herokuapp.com/facts"
+# r = requests.get(cat_url)
+r_obj_list = [
+  {
+    "_id": "58e008ad0aac31001185ed0c",
+    "text": "The frequency of a domestic cat;s purr is the same at which muscles and bones repair themselves.",
+    "type": "cat",
+    "user": {
+      "_id": "58e007480aac31001185ecef",
+      "name": {
+        "first": "Kasimir",
+        "last": "Schulz"
+      }
+    },
+    "upvotes": 6,
+    "userUpvoted": "None"
+  }, {
+    "_id": "58e008ad0aac31333385ed0c",
+    "text": "Los gatos caen parados bien vrgs.",
+    "type": "cat",
+    "user": {
+      "_id": "58e007480aac31001185ecef",
+      "name": {
+        "first": "Kasimir",
+        "last": "Schulz"
+      }
+    },
+    "upvotes": 6,
+    "userUpvoted": "None"
+  }
+]
 
 # Create an empty list to store individual facts in 
 # This will make it wasy to select a random one later
